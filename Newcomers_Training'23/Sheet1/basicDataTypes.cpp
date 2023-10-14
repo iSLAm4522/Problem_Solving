b0b2178d364840364ebcb4308a5985758a24c4c9
islam4522 <islammadian798@gmail.com>
2023-10-15 00:01:56 +0300

    data types

---
+++ Newcomers_Training'23/Sheet1/basicDataTypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int integer_number;
+long long long_number;
+char _character;
+float float_number;
+double double_number;
+cin>>integer_number>>long_number>>_character>>float_number>>double_number;
+cout<<integer_number<<'\n'<<long_number<<'\n'<<_character<<'\n'<<float_number<<'\n'<<double_number<<endl;
+return 0;
+}
